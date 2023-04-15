41349e4a682006c915bbdfe16b453c2d9919ef2a
Michota <dawidmichota@gmail.com>
2023-04-15 15:32:52 +0200

    Deafult style created, javascript file created

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+// Selecting elements
+
+// const result = document.querySelector(".cResult");
+// const previuousResult = document.querySelector(".cPesult");
+const result = document.querySelector(".cResult"),
+  previousResult = document.querySelector(".pResult");
